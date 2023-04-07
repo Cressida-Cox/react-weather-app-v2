f6a5290c8aa3de22a62715ef69043476db0d0c48
Cressida Cox <cox.cressie@yahoo.co.uk>
2023-04-07 14:36:59 +0100

    Error update

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       humidity: Math.round(response.data.main.humidity),
       wind: Math.round(response.data.wind.speed * 3.6),
       city: response.data.name,
-      iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       description: response.data.weather[0].description,
     });
   }
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -24,7 +24,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="WeatherForecast-date">{formatDay()}</div>
-      <WeatherIcon code={props.data.icon} size={36} />
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {" "}
